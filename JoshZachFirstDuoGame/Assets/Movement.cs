b049ec19d2d7c6f8b47560b3dcb0974099466926
JoshWix <juicewix@outlook.com>
2021-07-19 21:14:15 -0500

    Adds Movement
    Adds movement script and changes colors of floor and cube

--- JoshZachFirstDuoGame/Assets/Movement.cs
+++ JoshZachFirstDuoGame/Assets/Movement.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+
+    private Rigidbody rb;
+
+    private float moveX;
+    private float moveZ;
+
+    public float moveSpeed;
+
+    private bool jump;
+    public int jumpForce;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        moveX = Input.GetAxisRaw("Horizontal");
+        moveZ = Input.GetAxisRaw("Vertical");
+
+        rb.AddForce(moveX * moveSpeed, 0, moveZ * moveSpeed);
+
+
+
+        jump = Input.GetButtonDown("Jump");
+
+        if (jump) {
+            rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+        }
+
         
     }
 }
